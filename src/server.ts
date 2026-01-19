ad2155a494c295b326340a79b72d50f996f9557a
BL4KCY <mostafafarssiui@gmail.com>
2026-01-19 19:00:58 +0100

    add login route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
-import signupRoute from './routes/signup.js'
+import signUpRoute from './routes/signup'
+import signInRoute from './routes/login'
+import { jsonErrorHandler } from "./middlewares/application/global.error.handler";
 
 const PORT = 3000;
 
@@ -8,7 +10,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/auth', signupRoute);
+app.use(jsonErrorHandler);
+
+app.use('/auth', [
+	signUpRoute,
+	signInRoute
+])
 
 app.listen(PORT, () => {
 	console.log(`Authentication server is up and running in port: ${PORT}`)
