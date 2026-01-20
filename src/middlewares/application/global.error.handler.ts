997b64602e1f64176988cb219a4f8873392b5bc0
BL4KCY <mostafafarssiui@gmail.com>
2026-01-21 00:10:48 +0100

    add prisma error handler

--- src/middlewares/application/global.error.handler.ts
+++ src/middlewares/application/global.error.handler.ts
@@ -1,17 +1,40 @@
-import { ErrorRequestHandler,Request, Response, NextFunction } from "express";
+import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
+import { Prisma } from "../../lib/prisma";
 
 interface JsonSyntaxError extends SyntaxError {
-  status?: number;
-  body?: unknown;
+	status?: number;
+	body?: unknown;
 }
 
 
-export async function jsonErrorHandler(err: JsonSyntaxError, req: Request, res: Response, next: NextFunction) {
-	if (err instanceof SyntaxError && err.status === 400, 'body' in err ) {
+const jsonErrorHandler: ErrorRequestHandler = (err: JsonSyntaxError, req: Request, res: Response, next: NextFunction) => {
+
+	if (err instanceof SyntaxError && err.status === 400, 'body' in err) {
 		return res.status(400).json({
 			success: false,
 			error: 'invalid JSON body'
 		})
 	}
 	next(err);
+}
+
+const prismaErrorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof Prisma.PrismaClientKnownRequestError) {
+		if (err.code == 'P2002') {
+			res.status(500).json({
+				success: false,
+				message: "Resources Already Exist"
+			})
+		}
+		res.status(500).json({
+			success: false,
+			message: "database Error!"
+		})
+	}
+	next()
+}
+
+export {
+	jsonErrorHandler,
+	prismaErrorHandler
 }
