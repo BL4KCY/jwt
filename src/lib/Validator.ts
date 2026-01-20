87cb977601b75dcf643da45ae3774c05442a3725
BL4KCY <mostafafarssiui@gmail.com>
2026-01-21 00:10:31 +0100

    add jsonwebtoken format for Schema validator

--- src/lib/Validator.ts
+++ src/lib/Validator.ts
@@ -9,7 +9,8 @@ addErrors(ajv)
 const formats = {
 	username: /^[a-zA-Z][a-zA-Z0-9_.-]{2,19}$/,
 	password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/,
-	phoneNumber: /^\+?[1-9]\d{7,14}$/
+	phoneNumber: /^\+?[1-9]\d{7,14}$/,
+	jsonwebtoken: /e[yw][A-Za-z0-9-_]+\.(?:e[yw][A-Za-z0-9-_]+)?\.[A-Za-z0-9-_]{2,}(?:(?:\.[A-Za-z0-9-_]{2,}){2})?/
 }
 Object.entries(formats).forEach(([name, format]) => {
 	ajv.addFormat(name, format)
